bb308b2fdf704b21ea0934d8dba854fe50e17c2b
U2FsdGVkX1 <U2FsdGVkX1@gmail.com>
2024-07-30 13:45:29 +0800

    use LV_USE_PERF_MONITOR_POS instead

--- buildroot-overlay/package/lvgl/lv_conf.h
+++ buildroot-overlay/package/lvgl/lv_conf.h
@@ -253,7 +253,7 @@ extern uint32_t custom_tick_get(void);
 /*1: Show CPU usage and FPS count*/
 #define LV_USE_PERF_MONITOR 1
 #if LV_USE_PERF_MONITOR
-    #define LV_USE_PERF_MONITOR_POS LV_ALIGN_BOTTOM_RIGHT
+    #define LV_USE_PERF_MONITOR_POS LV_ALIGN_TOP_LEFT
 #endif
 
 /*1: Show the used memory and the memory fragmentation
